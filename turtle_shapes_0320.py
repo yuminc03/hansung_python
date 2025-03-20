17f980e2b3cfd3a56347047a97f4f7da26c689b3
Yumin Chu <yuminc03@gmail.com>
2025-03-20 22:33:56 +0900

    Feat: 터틀 프로그래밍 - 터틀 실습 (여러 도형 한 번에 그리기)

---
+++ turtle_shapes_0320.py
@@ -0,0 +1,24 @@
+import turtle
+t = turtle.Turtle()
+t.shape('turtle')
+# rectangle
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+
+# triangle
+t.left(180)
+t.left(30)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+
+# circle
+t.circle(-50)
+t.circle(100)
