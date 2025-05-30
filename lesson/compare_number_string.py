6f384951b3652641677e79d8939c0333f37e03db
Yumin Chu <yuminc03@gmail.com>
2025-05-30 18:21:05 +0900

    Feat: int와 string 타입 비교

---
+++ lesson/compare_number_string.py
@@ -0,0 +1,28 @@
+# a = 777
+# b = '777'
+# print(a+1)
+# print(b+'1')
+
+# print(type(123))
+# print(type('123'))
+# print(type(3.14))
+# print(type(True))
+
+# a = 123
+# print(type(a))
+# a = str(a)
+# print(type(a))
+
+# a = True
+# print(type(a))
+# a = int(a)
+# print(type(a))
+# print(a)
+
+
+# a = ''
+a = ' '
+print(type(a))
+a = bool(a)
+print(type(a))
+print(a)
