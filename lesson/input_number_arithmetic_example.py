6474a146a502bf059a6bab68da37429e265f9481
Yumin Chu <yuminc03@gmail.com>
2025-05-30 18:23:27 +0900

    Feat: input()으로 숫자를 입력받아서 사칙연산 출력

---
+++ lesson/input_number_arithmetic_example.py
@@ -0,0 +1,34 @@
+# print('입력해주세요 ')
+
+# a= input('입력: ')
+# print(a)
+
+# name = input('이름 : ')
+# subject = input('학과 : ')
+# address = input('주소 : ')
+# hobby = input('취미 : ')
+# print('****** 자기소개서 ******')
+# print('이름 : ', name)
+# print('학과 : ', subject)
+# print('주소 : ', address)
+# print('취미 : ', hobby)
+# print('*' * 27)
+
+# a = input('수 입력: ') # input으로 입력하는 것은 String 타입
+# print(type(a)) # <class 'str'>
+
+# a = int(input('수 입력: '))
+# print(type(a)) # <class 'int'>
+# print(a + 1)
+
+# a = input('수 입력: ')
+# print(type(a)) # <class 'str'>
+# print(int(a) + 1)
+
+# 문제
+num1 = int(input('첫 번째 수 입력: '))
+num2 = int(input('두 번째 수 입력: '))
+print(num1, ' + ', num2, '=', num1 + num2)
+print(num1, ' - ', num2, '=', num1 - num2)
+print(num1, ' * ', num2, '=', num1 * num2)
+print(num1, ' / ', num2, '=', num1 / num2)
