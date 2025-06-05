ee59633548ac38753d6436c278abff36c17aecc9
Yumin Chu <yuminc03@gmail.com>
2025-06-05 23:50:51 +0900

    Feat: random 모듈을 사용하여 랜덤의 수 뽑기

---
+++ lesson/random_module_example.py
@@ -0,0 +1,10 @@
+import random
+
+# a = random.randrange(1, 10) # 1부터 10-1까지의 수를 뽑기
+# print(a)
+
+# a = random.randrange(1, 7) # 주사위
+# print(a, "칸 이동")
+
+lotto = random.randrange(1, 46)
+print(lotto)
