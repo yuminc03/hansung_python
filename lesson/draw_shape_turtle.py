cb4195d80af631054119d38bfc9f7817b93e4ea4
Yumin Chu <yuminc03@gmail.com>
2025-06-05 23:48:36 +0900

    Feat: 거북이로 도형그리기

---
+++ lesson/draw_shape_turtle.py
@@ -0,0 +1,38 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape('turtle')
+
+# 사각형
+angle = 90
+t.fd(100)
+t.lt(angle)
+t.fd(100)
+t.lt(angle)
+t.fd(100)
+t.lt(angle)
+t.fd(200)
+
+# 오각형
+t.lt(72)
+t.fd(50)
+t.lt(72)
+t.fd(50)
+t.lt(72)
+t.fd(50)
+t.lt(72)
+t.fd(50)
+
+# 육각형
+t.fd(100)
+t.lt(60)
+t.fd(50)
+t.lt(60)
+t.fd(50)
+t.lt(60)
+t.fd(50)
+t.lt(60)
+t.fd(50)
+t.lt(60)
+t.fd(50)
+t.lt(60)
